c24868fe7ad6d810797366a3a23b88bddc5f2340
drimpact <drimpact@users.noreply.github.com>
2017-06-24 15:36:38 +0100

    Removing template containers

--- src/app/containers/index.tsx
+++ src/app/containers/index.tsx
@@ -1,8 +1,6 @@
 /** Exporting Containers for Easier Imports */
 export { App } from './App';
 export { Home } from './Home';
-export { Counter } from './Counter';
-export { Stars } from './Stars';
 export { Login } from './Login';
 export { Review } from './Review';
 export { Conduct } from './Conduct';
