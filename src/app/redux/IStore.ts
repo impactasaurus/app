6158cd858bc3ff1e6911aa71c76c17b7496c21bc
drimpact <drimpact@users.noreply.github.com>
2017-06-26 21:36:05 +0100

    Removing more bootstrap project left overs

--- src/app/redux/IStore.ts
+++ src/app/redux/IStore.ts
@@ -1,10 +1,6 @@
-import { ICounter } from 'models/counter';
-import { IStars } from 'models/stars';
 import { RouterState } from '@types/react-router-redux';
 
 export interface IStore {
-  counter: ICounter;
-  stars: IStars;
   routing: RouterState;
   reduxAsyncConnect: any;
   apollo: any;
--- src/app/redux/reducers.ts
+++ src/app/redux/reducers.ts
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { counterReducer } from './modules/counter';
-import { starsReducer } from './modules/stars';
 import { IStore } from './IStore';
 
 const { reducer } = require('redux-connect');
@@ -9,8 +7,6 @@ const { reducer } = require('redux-connect');
 export default function getReducers(clientReducers: Redux.ReducersMapObject): Redux.Reducer<IStore> {
   const internalReducers = {
     routing: routerReducer,
-    counter: counterReducer,
-    stars: starsReducer,
     reduxAsyncConnect: reducer,
   };
   return combineReducers<IStore>({...internalReducers, ...clientReducers});
