59eda162f8b1798f7cbb264324fd158a3529245d
drimpact <drimpact@users.noreply.github.com>
2017-06-26 22:13:47 +0100

    Cleaning up a little

--- config/webpack/dev.js
+++ config/webpack/dev.js
@@ -14,7 +14,12 @@ var config = {
 
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    modules: [path.resolve(__dirname), 'node_modules', 'app', 'app/redux'],
+    modules: [
+      path.resolve(__dirname),
+      'node_modules',
+      'app',
+      'app/redux'
+    ],
   },
 
   entry: {
