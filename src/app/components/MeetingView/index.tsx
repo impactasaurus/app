846e8bc22172d202c53cacd486ce0837c12468ec
drimpact <drimpact@users.noreply.github.com>
2017-06-29 20:31:39 +0100

    Adding a continue button on beneficiary's previous meetings if possible

--- src/app/components/MeetingView/index.tsx
+++ src/app/components/MeetingView/index.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import {getMeetings, IMeetingResult} from 'apollo/modules/meetings';
+import {IMeeting} from 'models/meeting';
 import {getOutcomeGraph} from '@ald-life/outcome-graph';
 import {Answer} from 'models/answer';
+import {IURLConnector} from 'redux/modules/url';
+import {renderArray} from 'helpers/react';
+import {setURL} from 'modules/url';
+import { bindActionCreators } from 'redux';
+const { connect } = require('react-redux');
 
 let count = 0;
 
-interface IProp {
+interface IProp extends IURLConnector {
   beneficiaryID: string;
   data?: IMeetingResult;
 }
 
+@connect(undefined, (dispatch) => ({
+  setURL: bindActionCreators(setURL, dispatch),
+}))
 class MeetingViewInner extends React.Component<IProp, any> {
 
   private canvasID: string;
@@ -17,6 +26,8 @@ class MeetingViewInner extends React.Component<IProp, any> {
   constructor(props) {
     super(props);
     this.canvasID = `meeting-view-${count++}`;
+    this.navigateToMeeting = this.navigateToMeeting.bind(this);
+    this.printMeetingControls = this.printMeetingControls.bind(this);
   }
 
   public componentDidUpdate() {
@@ -43,10 +54,36 @@ class MeetingViewInner extends React.Component<IProp, any> {
     getOutcomeGraph(this.canvasID, 'title', meeting);
   }
 
+  private navigateToMeeting(meeting: IMeeting): ()=>void {
+    return () => {
+      this.props.setURL(`/meeting/${meeting.id}`);
+    };
+  }
+
+  private canBeContinued(meeting: IMeeting) {
+    return meeting.answers.length < meeting.outcomeSet.questions.length;
+  }
+
+  private printMeetingControls(meeting: IMeeting) {
+    const buttons: JSX.Element[] = [];
+    if (this.canBeContinued(meeting)) {
+      buttons.push((<button key="cont" onClick={this.navigateToMeeting(meeting)}>Continue</button>));
+    }
+    return (
+      <div key={meeting.id}>
+        {new Date(meeting.conducted).toLocaleString()}
+        {buttons}
+      </div>
+    );
+  }
+
   public render() {
     return (
       <div>
-        <p>{this.props.beneficiaryID}</p>
+        <h3>Meetings:</h3>
+        {renderArray<IMeeting>(this.printMeetingControls, this.props.data.getMeetings)}
+        <hr />
+        <h3>Review:</h3>
         <div id="wrapper" style={{position: 'relative',height: '60vh', width:'60vw', margin:'auto'}}>
           <canvas id={this.canvasID} width={500} height={500} />
         </div>
