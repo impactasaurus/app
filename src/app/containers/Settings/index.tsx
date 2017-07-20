1da880a031337ea827191a7f4a0f3a4b26b46529
drimpact <drimpact@users.noreply.github.com>
2017-07-20 20:29:33 +0100

    Styled settings page. Closes #12.

--- src/app/containers/Settings/index.tsx
+++ src/app/containers/Settings/index.tsx
@@ -1,13 +1,48 @@
 import * as React from 'react';
-import { Link } from 'react-router';
+import { Menu } from 'semantic-ui-react';
+import {IURLConnector} from 'redux/modules/url';
+import {setURL} from 'modules/url';
+import { bindActionCreators } from 'redux';
+import {IStore} from 'redux/IStore';
+import './style.less';
+const { connect } = require('react-redux');
 
-class Settings extends React.Component<any, any> {
+interface IProps extends IURLConnector {
+  currentURL?: string;
+}
+
+@connect((state: IStore): IProps => {
+  return {
+    currentURL: state.routing.locationBeforeTransitions.pathname,
+  };
+}, (dispatch) => ({
+  setURL: bindActionCreators(setURL, dispatch),
+}))
+class Settings extends React.Component<IProps, any> {
+
+  constructor(props) {
+    super(props);
+    this.isSelected = this.isSelected.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  private isSelected(url: string): boolean {
+    return this.props.currentURL !== undefined && this.props.currentURL.includes(url);
+  }
+
+  private handleClick(url: string) {
+    return () => {
+      this.props.setURL(url);
+    };
+  }
+
   public render() {
     return (
       <div id="settings">
-        <ul>
-          <li><Link to="/settings/questions">Questions</Link></li>
-        </ul>
+        <Menu pointing secondary>
+          <Menu.Item name="Question Sets" active={this.isSelected('/settings/questions')} onClick={this.handleClick('/settings/questions')} />
+        </Menu>
+
         {this.props.children}
       </div>
     );
