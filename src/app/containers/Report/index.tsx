cbc996c48cd152742026a268a0f301d6298f0edb
drimpact <drimpact@users.noreply.github.com>
2017-09-01 21:57:44 +0100

    Improving styling and trying to add some explanation about what the report provides.

--- src/app/containers/Report/index.tsx
+++ src/app/containers/Report/index.tsx
@@ -4,9 +4,11 @@ import {IOutcomeSet} from 'models/outcomeSet';
 import {IURLConnector} from 'redux/modules/url';
 import {setURL} from 'modules/url';
 import { bindActionCreators } from 'redux';
-import { Button, Select, Grid, SelectProps } from 'semantic-ui-react';
+import { Button, Select, Grid, SelectProps, Message } from 'semantic-ui-react';
 import {DateRangePicker} from 'components/DateRangePicker';
+import {Hint} from 'components/Hint';
 const { connect } = require('react-redux');
+const strings = require('./../../../strings.json');
 
 interface IProp extends IURLConnector {
   data: IOutcomeResult;
@@ -36,7 +38,7 @@ class ReportInner extends React.Component<IProp, IState> {
 
   private validateInput(): string|null {
     if (!this.state.questionSetID || this.state.questionSetID === '') {
-      return 'Question set ID is required';
+      return 'A question set must be selected';
     }
     if (!this.state.periodStart || !this.state.periodEnd || this.state.periodStart >= this.state.periodEnd) {
       return 'Time range must be specified';
@@ -91,7 +93,7 @@ class ReportInner extends React.Component<IProp, IState> {
       <div className="impactform">
         <h3 className="label">Question Set</h3>
         <Select {...selectProps} placeholder="Question Set" onChange={this.setQuestionSetID} options={this.getOptions(outcomeSets)} />
-        <h3 className="label">Date Range</h3>
+        <h3 className="label"><Hint text={strings.JOCReportDateRange} />Date Range</h3>
         <DateRangePicker onSelect={this.setDateRange} future={false}/>
         <Button className="submit" onClick={this.navigateToReport}>Generate</Button>
         <p>{this.state.error}</p>
@@ -104,7 +106,16 @@ class ReportInner extends React.Component<IProp, IState> {
     return (
       <Grid container columns={1} id="report">
         <Grid.Column>
-          <h1>Report</h1>
+          <h1>Service Report</h1>
+          <Message info>
+            <Message.Header>What does this report provide?</Message.Header>
+            <p>The service report quantifies your organisation's impact on your beneficiaries.</p>
+            <p>
+              This is calculated by examining how much change there has been between each beneficiary's first ever recorded assessment (may be outside of the specified date range) and their most recent assessment within the provided time range.
+              The average measurements from the first and most recent assessments, across all suitable beneficiaries, are compared and visualised.
+            </p>
+            <p>This report will only include beneficiaries with more than one assessment, so that the amount of change can be understood.</p>
+          </Message>
           {this.renderNewReportControl(data.allOutcomeSets)}
         </Grid.Column>
       </Grid>
