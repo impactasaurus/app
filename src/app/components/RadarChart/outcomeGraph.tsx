7ad0fa06b6263db3783e7d6003cf79a68a6449da
drimpact <drimpact@users.noreply.github.com>
2017-09-03 13:11:51 +0100

    Moving github.com/ALDLife/outcome-graph into project

---
+++ src/app/components/RadarChart/outcomeGraph.tsx
@@ -0,0 +1,48 @@
+// originated from github.com/ALDLife/outcome-graph
+
+import * as Chart from 'chart.js';
+import {SessionsConverter} from './sessionsConverter';
+const sessionsConverter = SessionsConverter();
+
+export function getOutcomeGraph(canvasDiv, title, data) {
+  const chartConfig = getConfig(data, title);
+  const canvasElement = document.getElementById(canvasDiv);
+  if (canvasElement === null) {
+    throw new Error('The canvas element specified does not exist!');
+  }
+
+  return new Chart(document.getElementById(canvasDiv), chartConfig);
+}
+
+function getConfig(data, title) {
+  return {
+    type: 'radar',
+    data: sessionsConverter.getChartJSConvertedData(data),
+    options: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: title,
+      },
+      scale: {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+      tooltips: {
+        enabled: true,
+        callbacks: {
+          label: function label(tooltipItem, chartData) {
+            const datasetLabel = chartData.datasets[tooltipItem.datasetIndex].label || '';
+            // This will be the tooltip.body
+            return datasetLabel + ' : ' + tooltipItem.yLabel + ' : ' + chartData.datasets[tooltipItem.datasetIndex].notes[tooltipItem.index];
+          },
+        },
+      },
+    },
+  };
+}
+
+module.exports.getOutcomeGraph = getOutcomeGraph;
---
+++ src/app/components/ServiceReportRadar/index.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import {IJOCServiceReport, IQuestionAggregates, ICategoryAggregates} from 'models/report';
+import {IOutcomeSet} from 'models/outcomeSet';
+import {RadarChart} from 'components/RadarChart';
+import {RadarData, IRadarSeries, IRadarPoint} from 'models/radar';
+
+interface IProp {
+  serviceReport: IJOCServiceReport;
+  questionSet: IOutcomeSet;
+  category?: boolean;
+}
+
+class ServiceReportRadar extends React.Component<IProp, any> {
+
+  private getCategoryRadarData(p: IProp): RadarData {
+    const getCatRadarSeries = (c: ICategoryAggregates[], name: string): IRadarSeries => {
+      return {
+        name,
+        datapoints: c.map((point: ICategoryAggregates): IRadarPoint => {
+          let category = 'Unknown Category';
+          const cat = p.questionSet.categories.find((c) => c.id === point.categoryID);
+          if (cat !== undefined) {
+            category = cat.name;
+          }
+          return {
+            axis: category,
+            value: point.value,
+          };
+        }),
+      };
+    };
+    return [getCatRadarSeries(p.serviceReport.categoryAggregates.first, 'initial'), getCatRadarSeries(p.serviceReport.categoryAggregates.last, 'latest')];
+  }
+
+  private getQuestionRadarData(p: IProp): RadarData {
+    const getQRadarSeries = (c: IQuestionAggregates[], name: string): IRadarSeries => {
+      return {
+        name,
+        datapoints: c.map((point: IQuestionAggregates): IRadarPoint => {
+          const q = p.questionSet.questions.find((q) => q.id === point.questionID);
+          let question = 'Unknown Question';
+          if (q !== undefined) {
+            if (q.archived) {
+              question = `${q.question} (archived)`;
+            } else {
+              question = q.question;
+            }
+          }
+          return {
+            axis: question,
+            value: point.value,
+          };
+        }),
+      };
+    };
+    return [getQRadarSeries(p.serviceReport.questionAggregates.first, 'initial'), getQRadarSeries(p.serviceReport.questionAggregates.last, 'latest')];
+  }
+
+  private getRadarData(p: IProp): RadarData {
+    if (p.category) {
+      return this.getCategoryRadarData(p);
+    }
+    return this.getQuestionRadarData(p);
+  }
+
+  public render() {
+    return (
+      <div className="service-report-radar">
+        <RadarChart data={this.getRadarData(this.props)} />
+      </div>
+    );
+  }
+}
+
+export {ServiceReportRadar}
