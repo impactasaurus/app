2faca680673d6fc7d93f1fc2d75456d11cd49ac3
Nikhil Warekar <nik_w@tuta.io>
2017-09-12 17:24:29 -0400

    Organization - Nest Helmet title inside Grid.Column

--- src/app/containers/Organisation/index.tsx
+++ src/app/containers/Organisation/index.tsx
@@ -6,18 +6,16 @@ class Organisation extends React.Component<any, any> {
 
   public render() {
     return (
-      <div>
-        <Helmet>
-          <title>Organisation</title>
-        </Helmet>
         <Grid container columns={1} id="organisation">
           <Grid.Column>
+            <Helmet>
+              <title>Organisation</title>
+            </Helmet>
             <h1>Organisation</h1>
             <h3>Users</h3>
             <p>To add users to your organisation, please send their email addresses to <a href="mailto:support@impactasaurus.org?Subject=NewUsers">support@impactasaurus.org</a></p>
           </Grid.Column>
         </Grid>
-      </div>
     );
   }
 }
