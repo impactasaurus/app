f69e48f36adf098b57febf6ed95e7b0373a27c18
drimpact <drimpact@users.noreply.github.com>
2017-09-14 22:14:13 +0100

    Improving new likert question component. Instead of just having meaningless input boxes, it is now contextualized by positioning around a likert scale. Closes #27 and closes #47.

--- src/app/components/NewLikertQuestion/index.tsx
+++ src/app/components/NewLikertQuestion/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { Button, ButtonProps } from 'semantic-ui-react';
+import { Button, ButtonProps, Message } from 'semantic-ui-react';
 import {Input} from 'components/Input';
 import {IQuestionMutation, addLikertQuestion} from 'apollo/modules/questions';
+import { Likert} from 'components/Likert';
+import './style.less';
 
 interface IProps extends IQuestionMutation {
   QuestionSetID: string;
@@ -13,6 +15,8 @@ interface IState {
   newQuestion?: string;
   leftLabel?: string;
   rightLabel?: string;
+  leftValue?: string;
+  rightValue?: string;
   saving?: boolean;
 }
 
@@ -22,12 +26,26 @@ class NewLikertQuestionInner extends React.Component<IProps, IState> {
     super(props);
     this.state = {
       saving: false,
+      leftValue: '0',
+      rightValue: '10',
     };
     this.addQuestion = this.addQuestion.bind(this);
     this.setNewQuestion = this.setNewQuestion.bind(this);
     this.setLeftLabel = this.setLeftLabel.bind(this);
     this.setRightLabel = this.setRightLabel.bind(this);
+    this.setLeftValue = this.setLeftValue.bind(this);
+    this.setRightValue = this.setRightValue.bind(this);
   }
+
+  private isInt(str: string): boolean {
+    const n = Number(str);
+    const i = Math.floor(n);
+    return !Number.isNaN(i) && i === n;
+  }
+
+  private leftValueInt = (s: IState) => parseInt(s.leftValue, 10);
+  private rightValueInt = (s: IState) => parseInt(s.rightValue, 10);
+  private noop = () => {};
 
   private addQuestion() {
     if (typeof this.state.newQuestion !== 'string' || this.state.newQuestion.length === 0) {
@@ -36,10 +54,24 @@ class NewLikertQuestionInner extends React.Component<IProps, IState> {
       });
       return;
     }
+    if (!this.isInt(this.state.leftValue) || !this.isInt(this.state.rightValue)) {
+      this.setState({
+        newQuestionError: 'The left and right values must be whole integers',
+      });
+      return;
+    }
+    const lv = this.leftValueInt(this.state);
+    const rv = this.rightValueInt(this.state);
+    if (lv === rv) {
+      this.setState({
+        newQuestionError: 'The left and right values cannot be equal',
+      });
+      return;
+    }
     this.setState({
       saving: true,
     });
-    this.props.addLikertQuestion(this.props.QuestionSetID, this.state.newQuestion, 10, this.state.leftLabel, this.state.rightLabel)
+    this.props.addLikertQuestion(this.props.QuestionSetID, this.state.newQuestion, lv, rv, this.state.leftLabel, this.state.rightLabel)
     .then(() => {
       this.setState({
         saving: false,
@@ -72,6 +104,18 @@ class NewLikertQuestionInner extends React.Component<IProps, IState> {
     });
   }
 
+  private setLeftValue(_, data) {
+    this.setState({
+      leftValue: data.value,
+    });
+  }
+
+  private setRightValue(_, data) {
+    this.setState({
+      rightValue: data.value,
+    });
+  }
+
   public render() {
     const addProps: ButtonProps = {};
     if (this.state.saving) {
@@ -79,13 +123,35 @@ class NewLikertQuestionInner extends React.Component<IProps, IState> {
       addProps.disabled = true;
     }
     return (
-      <div>
-        <Input autofocus type="text" placeholder="Question" onChange={this.setNewQuestion} />
-        <Input type="text" placeholder="Left Label" onChange={this.setLeftLabel} />
-        <Input type="text" placeholder="Right Label" onChange={this.setRightLabel} />
-        <Button {...addProps} onClick={this.addQuestion}>Add</Button>
-        <p>{this.state.newQuestionError}</p>
-      </div>
+      <Message>
+        <Message.Content>
+          <Message.Header>New Likert Question</Message.Header>
+          <div className="new-likert-form">
+            <div className="section upper">
+              <Input className="full" autofocus type="text" placeholder="Question" onChange={this.setNewQuestion} />
+            </div>
+            <div className="section mid">
+              <Input className="left" type="text" placeholder="Left Label (optional)" onChange={this.setLeftLabel} />
+              <Input className="right" type="text" placeholder="Right Label (optional)" onChange={this.setRightLabel} />
+            </div>
+            <div className="section likert">
+              <Likert
+                leftValue={this.leftValueInt(this.state)}
+                rightValue={this.rightValueInt(this.state)}
+                onChange={this.noop}
+                disabled={true} />
+            </div>
+            <div className="section lower">
+              <Input className="left" type="number" placeholder="Left Value" onChange={this.setLeftValue} defaultValue={this.state.leftValue} />
+              <Input className="right" type="number" placeholder="Right Value" onChange={this.setRightValue} defaultValue={this.state.rightValue} />
+            </div>
+            <div className="controls">
+              <Button {...addProps} onClick={this.addQuestion}>Add</Button>
+              <p>{this.state.newQuestionError}</p>
+            </div>
+          </div>
+        </Message.Content>
+      </Message>
     );
   }
 }
