6b6e82bd6b0bbaee8d7f3233e2525400b7827f32
drimpact <drimpact@users.noreply.github.com>
2017-09-23 22:09:17 +0100

    Introduced meeting table and radar components. Wired into the review page. Added question set selector to review page. Code needs a lot of work still.

--- src/app/containers/Review/index.tsx
+++ src/app/containers/Review/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { Grid, Loader } from 'semantic-ui-react';
+import { Grid, Loader, Select, DropdownItemProps } from 'semantic-ui-react';
 import {VizControlPanel} from 'components/VizControlPanel';
 const { connect } = require('react-redux');
 import {IStore} from 'redux/IStore';
 import {Aggregation, Visualisation, getAggregation, getVisualisation} from 'models/pref';
 import {getMeetings, IMeetingResult} from 'apollo/modules/meetings';
 import {IMeeting} from 'models/meeting';
+import {MeetingRadar} from 'components/MeetingRadar';
+import {MeetingTable} from 'components/MeetingTable';
 import './style.less';
 
 interface IProps {
@@ -18,6 +20,10 @@ interface IProps {
   data?: IMeetingResult;
   isCategoryAgPossible?: boolean;
 };
+
+interface IState {
+  selectedQuestionSetID: string;
+}
 
 function isCategoryAggregationAvailable(meetings: IMeeting[]): boolean {
   if (!Array.isArray(meetings) || meetings.length === 0) {
@@ -37,19 +43,78 @@ function isCategoryAggregationAvailable(meetings: IMeeting[]): boolean {
     isCategoryAgPossible: canCatAg,
   };
 }, undefined)
-class ReviewInner extends React.Component<IProps, any> {
+class ReviewInner extends React.Component<IProps, IState> {
+
   constructor(props) {
     super(props);
+    this.state = {
+      selectedQuestionSetID: null,
+    };
+    this.renderInner = this.renderInner.bind(this);
+    this.renderVis = this.renderVis.bind(this);
+    this.setQuestionSetID = this.setQuestionSetID.bind(this);
   }
 
-  private renderVis(p: IProps): JSX.Element {
-    if (p.vis === Visualisation.RADAR) {
-      return (<div>RADAR</div>);
+  public componentWillReceiveProps(nextProps: IProps) {
+    const meetings = nextProps.data.getMeetings;
+    if(this.state.selectedQuestionSetID === null &&
+      Array.isArray(meetings) &&
+      meetings.length > 0) {
+        this.setState({
+          selectedQuestionSetID: meetings[0].outcomeSetID,
+        });
+      }
+  }
+
+  private filterMeetings(m: IMeeting[], questionSetID: string): IMeeting[] {
+    return m.filter((m) => m.outcomeSetID === questionSetID);
+  }
+
+  private renderVis(): JSX.Element {
+    const p = this.props;
+    if (!Array.isArray(p.data.getMeetings) || p.data.getMeetings.length === 0) {
+      return (
+        <p>No meetings found for beneficiary {p.params.id}</p>
+      );
     }
-    return (<div>TABLE</div>);
+
+    const meetings = this.filterMeetings(p.data.getMeetings, this.state.selectedQuestionSetID);
+
+    if (p.vis === Visualisation.RADAR) {
+      return (
+        <MeetingRadar aggregation={p.agg} meetings={meetings} />
+      );
+    }
+    return (
+      <MeetingTable aggregation={p.agg} meetings={meetings} />
+    );
   }
 
-  private renderInner(p: IProps): JSX.Element {
+  private getQuestionSetOptions(ms: IMeeting[]): DropdownItemProps[] {
+    if (!Array.isArray(ms)) {
+      return [];
+    }
+    const questionSetMap = ms.reduce((qs, m) => {
+      if (qs[m.outcomeSetID] !== undefined) {
+        return qs;
+      }
+      qs[m.outcomeSetID] = {
+        key: m.outcomeSetID,
+        value: m.outcomeSetID,
+        text: m.outcomeSet.name,
+      };
+      return qs;
+    }, {});
+    return Object.keys(questionSetMap).map((qsID) => questionSetMap[qsID]);
+  }
+
+  private setQuestionSetID(_, data) {
+    this.setState({
+      selectedQuestionSetID: data.value,
+    });
+  }
+
+  private renderInner(): JSX.Element {
     if (this.props.data.loading) {
       return (
         <Loader active={true} inline="centered" />
@@ -58,7 +123,12 @@ class ReviewInner extends React.Component<IProps, any> {
     return (
       <div>
         <VizControlPanel canCategoryAg={this.props.isCategoryAgPossible} />
-        {this.renderVis(p)}
+        <Select className="qs-selector"
+          value={this.state.selectedQuestionSetID}
+          onChange={this.setQuestionSetID}
+          options={this.getQuestionSetOptions(this.props.data.getMeetings)}
+        />
+        {this.renderVis()}
       </div>
     );
   }
@@ -76,7 +146,7 @@ class ReviewInner extends React.Component<IProps, any> {
               <title>{this.props.params.id + ' Review'}</title>
             </Helmet>
             <h1>{this.props.params.id}</h1>
-            {this.renderInner(this.props)}
+            {this.renderInner()}
           </div>
         </Grid.Column>
       </Grid>
