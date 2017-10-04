ef3d654b1b1e308b337b9fc3a127c5f13794d958
Nikhil Warekar <nik_w@tuta.io>
2017-10-03 21:30:26 -0400

    Wrapped relevant elements in Form element to submit benficiary ID via Enter Key

--- src/app/containers/ReviewSelector/index.tsx
+++ src/app/containers/ReviewSelector/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { Button, Input, Grid } from 'semantic-ui-react';
+import { Button, Input, Grid, Form } from 'semantic-ui-react';
 import './style.less';
-import {setURL} from 'modules/url';
+import { setURL } from 'modules/url';
 import { bindActionCreators } from 'redux';
-import {IURLConnector} from 'redux/modules/url';
-import {Hint} from 'components/Hint';
+import { IURLConnector } from 'redux/modules/url';
+import { Hint } from 'components/Hint';
 const { connect } = require('react-redux');
 const strings = require('./../../../strings.json');
 
@@ -23,6 +23,7 @@ class ReviewSelector extends React.Component<IURLConnector, IState> {
     this.state = {};
     this.review = this.review.bind(this);
     this.setBenID = this.setBenID.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   private review() {
@@ -36,6 +37,10 @@ class ReviewSelector extends React.Component<IURLConnector, IState> {
     });
   }
 
+  private handleSubmit() {
+    this.review();
+  }
+
   public render() {
     return (
       <Grid container columns={1} >
@@ -45,9 +50,11 @@ class ReviewSelector extends React.Component<IURLConnector, IState> {
             <title>Review</title>
           </Helmet>
           <h1>Review</h1>
-          <span className="label"><Hint text={strings.beneficiaryIDExplanation} /><h3>Beneficiary ID</h3></span>
-          <Input type="text" placeholder="Beneficiary ID" onChange={this.setBenID}/>
-          <Button className="submit" onClick={this.review}>Review</Button>
+          <Form onSubmit={this.handleSubmit}>
+            <span className="label"><Hint text={strings.beneficiaryIDExplanation} /><h3>Beneficiary ID</h3></span>
+            <Input type="text" placeholder="Beneficiary ID" onChange={this.setBenID}/>
+            <Button className="submit" onClick={this.review}>Review</Button>
+          </Form>
         </div>
         {this.props.children}
         </Grid.Column>
