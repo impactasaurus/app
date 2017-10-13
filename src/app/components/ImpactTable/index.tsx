d588d62b697a9ecfb59d5db36582050ea0026afc
Nikhil Warekar <nik_w@tuta.io>
2017-10-13 10:18:42 -0400

    Added 'total' and 'average' rows to ImpactTable

--- src/app/components/ImpactTable/index.tsx
+++ src/app/components/ImpactTable/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Table} from 'semantic-ui-react';
-import {renderArray} from 'helpers/react';
+import { Table, Header } from 'semantic-ui-react';
+import { renderArray } from 'helpers/react';
 
 interface IRow {
   name: string;
@@ -44,11 +44,48 @@ class ImpactTable extends React.Component<IProp, any> {
     });
   }
 
+  private sumInitialColumn(rows: IRow[]): number | undefined {
+    let initialColumnSum: number = 0;
+    for(const row of rows) {
+      if(row.first === undefined) {
+        return undefined;
+      }
+      initialColumnSum += row.first;
+    }
+    return initialColumnSum;
+  }
+
+  private sumLatestColumn(rows: IRow[]): number | undefined {
+    let latestColumnSum: number = 0;
+    for(const row of rows) {
+      if(row.last === undefined) {
+        return undefined;
+      }
+      latestColumnSum += row.last;
+    }
+    return latestColumnSum;
+  }
+
+  private sumDifferenceColumn(rows: IRow[]): number | undefined {
+    let differenceColumnSum: number = 0;
+    for(const row of rows) {
+      const d = delta(row);
+      if(d === undefined) {
+        return undefined;
+      }
+      differenceColumnSum += d;
+    }
+    return differenceColumnSum;
+  }
+
   private renderTable(p: IProp): JSX.Element {
     if (p.data.length === 0) {
       return (<div />);
     }
     const rows = this.sortByDelta(p.data);
+    const initialColumnSum = this.sumInitialColumn(rows);
+    const latestColumnSum = this.sumLatestColumn(rows);
+    const differenceColumnSum = this.sumDifferenceColumn(rows);
     return (
       <Table striped={true} celled={true}>
         <Table.Header>
@@ -63,6 +100,23 @@ class ImpactTable extends React.Component<IProp, any> {
         <Table.Body>
           {renderArray(this.renderRow, rows)}
         </Table.Body>
+
+        <Table.Row>
+            <Table.Cell>
+              <Header as="h5" textAlign="left">Total</Header>
+            </Table.Cell>
+            <Table.Cell>{initialColumnSum.toFixed(2)}</Table.Cell>
+            <Table.Cell>{latestColumnSum.toFixed(2)}</Table.Cell>
+            <Table.Cell>{differenceColumnSum.toFixed(2)}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+            <Table.Cell>
+              <Header as="h5" textAlign="left">Average</Header>
+            </Table.Cell>
+            <Table.Cell>{initialColumnSum === undefined ? 'N/A' : (initialColumnSum/rows.length).toFixed(2)}</Table.Cell>
+            <Table.Cell>{latestColumnSum === undefined ? 'N/A' : (latestColumnSum/rows.length).toFixed(2)}</Table.Cell>
+            <Table.Cell>{differenceColumnSum === undefined ? 'N/A' : (differenceColumnSum/rows.length).toFixed(2)}</Table.Cell>
+        </Table.Row>
       </Table>
     );
   }
