7270d2e8828f4dc89834035b7813f5532462deaa
drimpact <drimpact@users.noreply.github.com>
2018-03-27 21:27:29 +0100

    allowing notes to be captured against answers

---
+++ src/app/components/Notepad/index.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import {Icon, Form, Accordion, TextArea, TextAreaProps} from 'semantic-ui-react';
+import './style.less';
+
+interface IProps {
+  notes: string | undefined | null;
+  onChange: (notes: string) => void;
+  placeholder?: string;
+}
+
+interface IState {
+  open: boolean;
+}
+
+class Notepad extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      open: props.notes !== undefined && props.notes !== null && props.notes.length > 0,
+    };
+    this.onChange = this.onChange.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  private onChange(_: React.FormEvent<HTMLTextAreaElement>, data: TextAreaProps): void {
+    if (typeof data.value === 'string') {
+      this.props.onChange(data.value);
+    }
+  }
+
+  private toggleOpen() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  public render() {
+    const placeholder = this.props.placeholder || 'Please enter any additional notes';
+    const notesNotNull: string | undefined = this.props.notes ? this.props.notes : undefined;
+    return (
+      <Accordion className="notepad">
+        <Accordion.Title className="accordion" active={this.state.open} index={0} onClick={this.toggleOpen}>
+          <Icon name="dropdown" />
+          Notes
+        </Accordion.Title>
+        <Accordion.Content active={this.state.open}>
+          <Form>
+            <TextArea autoHeight placeholder={placeholder} rows={2} onChange={this.onChange} value={notesNotNull} />
+          </Form>
+        </Accordion.Content>
+      </Accordion>
+    );
+  }
+}
+
+export { Notepad };
