2f81eefa03913fa960f6a63d3017987dcefcde85
drimpact <drimpact@users.noreply.github.com>
2018-04-08 21:47:54 +0100

    splitting edit likert request into two, this helps towards the caching issues and removes a lot of complexity around clearing update and refetch

--- src/app/apollo/modules/questions/index.ts
+++ src/app/apollo/modules/questions/index.ts
@@ -1,7 +1,6 @@
 import {gql, graphql} from 'react-apollo';
 import {IOutcomeSet, fragment as osFragment} from 'models/outcomeSet';
 import {mutationResultExtractor} from 'helpers/apollo';
-import {clearCacheOfAllMeetings} from 'apollo/modules/meetings';
 
 export function addLikertQuestion<T>(component) {
   return graphql<any, T>(gql`
@@ -30,20 +29,14 @@ export function addLikertQuestion<T>(component) {
 
 export function editLikertQuestion<T>(component) {
   return graphql<any, T>(gql`
-  mutation ($outcomeSetID: ID!, $questionID: ID!, $question: String!, $description: String, $minLabel: String, $maxLabel: String, $categoryID: ID) {
+  mutation ($outcomeSetID: ID!, $questionID: ID!, $question: String!, $description: String, $minLabel: String, $maxLabel: String) {
     editLikertQuestion: EditLikertQuestion(outcomeSetID:$outcomeSetID, questionID: $questionID, question: $question, description: $description, minLabel: $minLabel, maxLabel: $maxLabel) {
       ...defaultOutcomeSet
     },
-    setCategory: SetCategory(outcomeSetID: $outcomeSetID, questionID: $questionID, categoryID: $categoryID) {
-      ...defaultOutcomeSet
-    }
   }
   ${osFragment}`, {
-    options: {
-      refetchQueries: ['getOutcomeSet'],
-    },
     props: ({ mutate }) => ({
-      editLikertQuestion: (outcomeSetID: string, questionID: string, question: string, minLabel?: string, maxLabel?: string, description?: string, categoryID?: string): Promise<IOutcomeSet> => mutate({
+      editLikertQuestion: (outcomeSetID: string, questionID: string, question: string, minLabel?: string, maxLabel?: string, description?: string): Promise<IOutcomeSet> => mutate({
         variables: {
           outcomeSetID,
           questionID,
@@ -51,10 +44,8 @@ export function editLikertQuestion<T>(component) {
           description,
           minLabel,
           maxLabel,
-          categoryID,
         },
-        update: clearCacheOfAllMeetings(),
-      }).then(mutationResultExtractor<IOutcomeSet>('setCategory')),
+      }).then(mutationResultExtractor<IOutcomeSet>('editLikertQuestion')),
     }),
   })(component);
 }
@@ -80,6 +71,6 @@ export function deleteQuestion<T>(component) {
 
 export interface IQuestionMutation {
   addLikertQuestion?(outcomeSetID: string, question: string, minValue: number, maxValue: number, minLabel?: string, maxLabel?: string, description?: string, categoryID?: string): Promise<IOutcomeSet>;
-  editLikertQuestion?(outcomeSetID: string, questionID: string, question: string, minLabel?: string, maxLabel?: string, description?: string, categoryID?: string): Promise<IOutcomeSet>;
+  editLikertQuestion?(outcomeSetID: string, questionID: string, question: string, minLabel?: string, maxLabel?: string, description?: string): Promise<IOutcomeSet>;
   deleteQuestion?(outcomeSetID: string, questionID: string): Promise<IOutcomeSet>;
 }
--- src/app/components/EditLikertQuestion/index.tsx
+++ src/app/components/EditLikertQuestion/index.tsx
@@ -3,8 +3,9 @@ import {IQuestionMutation, editLikertQuestion} from 'apollo/modules/questions';
 import {IOutcomeSet} from 'models/outcomeSet';
 import {LikertQuestionForm} from '../LikertQuestionForm/index';
 import {ILikertQuestionForm, Question} from 'models/question';
+import {ICategoryMutation, setCategory} from '../../apollo/modules/categories';
 
-interface IProps extends IQuestionMutation {
+interface IProps extends IQuestionMutation, ICategoryMutation {
   QuestionSetID: string;
   OnSuccess: ()=>void;
   question: Question;
@@ -18,7 +19,13 @@ class EditLikertQuestionInner extends React.Component<IProps, any> {
   }
 
   private editQuestion(q: ILikertQuestionForm): Promise<IOutcomeSet> {
-    return this.props.editLikertQuestion(this.props.QuestionSetID, this.props.question.id, q.question, q.minLabel, q.maxLabel, q.description, q.categoryID);
+    let p = this.props.editLikertQuestion(this.props.QuestionSetID, this.props.question.id, q.question, q.minLabel, q.maxLabel, q.description);
+    if (q.categoryID !== this.props.question.categoryID) {
+      p = p.then(() => {
+        return this.props.setCategory(this.props.QuestionSetID, this.props.question.id, q.categoryID);
+      });
+    }
+    return p;
   }
 
   public render() {
@@ -42,5 +49,5 @@ class EditLikertQuestionInner extends React.Component<IProps, any> {
   }
 }
 
-const EditLikertQuestion = editLikertQuestion<IProps>(EditLikertQuestionInner);
+const EditLikertQuestion = setCategory<IProps>(editLikertQuestion<IProps>(EditLikertQuestionInner));
 export { EditLikertQuestion };
