5609b9954e879eacd7598872fdd453724e678e8a
drimpact <drimpact@users.noreply.github.com>
2018-05-01 22:32:15 +0100

    adding suggestTag query

--- src/app/apollo/modules/tags/index.ts
+++ src/app/apollo/modules/tags/index.ts
@@ -1,4 +1,6 @@
 import {gql, graphql, QueryProps} from 'react-apollo';
+import {IDExtractor} from '../../../helpers/apollo';
+import {isNullOrUndefined} from 'util';
 
 export const getTags = <T>(component)  => {
   return graphql<any, T>(gql`
@@ -15,4 +17,27 @@ export const getTags = <T>(component)  => {
 
 export interface ITagResult extends QueryProps {
   getTags?: string[];
+}
+
+export const suggestTags = <T>(idExtractor: IDExtractor<T>) => {
+  return graphql<any, T>(gql`
+    query suggestTags($ben: String!) {
+      suggestTags: suggestTags(beneficiary:$ben)
+    }`,
+    {
+      options: (props: T) => {
+        const ben = idExtractor(props);
+        return {
+          variables: {
+            ben,
+          },
+          skip: ben === '' || isNullOrUndefined(ben),
+        };
+      },
+    },
+  );
+};
+
+export interface ISuggestTagsResult extends QueryProps {
+  suggestTags?: string[];
 }
