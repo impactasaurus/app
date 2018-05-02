32ed4bbd725e6f735e2b47d030794174ee464b96
drimpact <drimpact@users.noreply.github.com>
2018-05-02 16:57:39 +0100

    not suggesting already selected tags. adding message when all tags have been selected. fixed suggestion icon styling

--- src/app/components/RecordTagInputWithSuggestions/index.tsx
+++ src/app/components/RecordTagInputWithSuggestions/index.tsx
@@ -15,44 +15,64 @@ interface IProps {
   tags: string[];
 }
 
-function renderTag(tag: string): JSX.Element {
-  return(
-    <Label as="a" key={tag}>
-      {tag}
-      <Icon name="add"/>
-    </Label>
-  );
-}
-
-function renderSuggested(props: IProps): JSX.Element {
-  if (isNullOrUndefined(props.data)) {
-    return (<div />);
-  }
-  const wrap = (inner: JSX.Element): JSX.Element => {
-    return ((
-      <div>
-        <h4 className="label">Suggested Tags</h4>
-        {inner}
-      </div>
-    ));
-  };
-  if (isNullOrUndefined(props.data.error) === false) {
-    return wrap(<span className="deemph">Failed to load suggested tags - sorry</span>);
-  }
-  if (props.data.loading) {
-    return wrap(<span className="deemph">Loading...</span>);
-  }
-  if (props.data.suggestTags.length === 0) {
-    return wrap(<span className="deemph">No suggested tags for this beneficiary</span>);
-  }
-  return wrap((
-    <span>
-      {renderArray<string>(renderTag, props.data.suggestTags)}
-    </span>
-  ));
+function filterAlreadySelectedTags(suggested: string[], selected: string[]) {
+  return suggested.filter((t) => selected.indexOf(t) === -1);
 }
 
 class RecordTagInputWithSuggestionsInner extends React.Component<IProps, any> {
+
+  constructor(props) {
+    super(props);
+    this.renderTag = this.renderTag.bind(this);
+    this.renderSuggested = this.renderSuggested.bind(this);
+    this.addTag = this.addTag.bind(this);
+  }
+
+  private addTag(t: string): () => void {
+    return () => this.props.onChange(this.props.tags.concat(t));
+  }
+
+  private renderTag(tag: string): JSX.Element {
+    return(
+      <Label as="a" key={tag} onClick={this.addTag(tag)}>
+        {tag}
+        <Icon name="plus"/>
+      </Label>
+    );
+  }
+
+  private renderSuggested(props: IProps): JSX.Element {
+    if (isNullOrUndefined(props.data)) {
+      return (<div />);
+    }
+    const wrap = (inner: JSX.Element): JSX.Element => {
+      return ((
+        <div>
+          <h4 className="label">Suggested Tags</h4>
+          {inner}
+        </div>
+      ));
+    };
+    if (isNullOrUndefined(props.data.error) === false) {
+      return wrap(<span>Failed to load suggested tags - sorry</span>);
+    }
+    if (props.data.loading) {
+      return wrap(<span>Loading...</span>);
+    }
+    if (props.data.suggestTags.length === 0) {
+      return wrap(<span>No suggested tags available for this beneficiary</span>);
+    }
+    const unselectedSuggestedTags = filterAlreadySelectedTags(props.data.suggestTags, props.tags);
+    if (unselectedSuggestedTags.length === 0) {
+      return wrap(<span>All suggested tags selected</span>);
+    }
+    return wrap((
+      <span>
+      {renderArray<string>(this.renderTag, unselectedSuggestedTags)}
+    </span>
+    ));
+  }
+
   public render() {
     return (
       <div className="record-tag-input-w-suggestions">
@@ -61,7 +81,7 @@ class RecordTagInputWithSuggestionsInner extends React.Component<IProps, any> {
           tags={this.props.tags}
           allowNewTags={this.props.allowNewTags}
         />
-        {renderSuggested(this.props)}
+        {this.renderSuggested(this.props)}
       </div>
     );
   }
