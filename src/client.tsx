cd769e82a67a71fcf8fb588c10b5777fa5e289fb
drimpact <drimpact@users.noreply.github.com>
2018-07-04 19:44:53 +0100

    using babel polyfill

--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,5 @@
-import * as e6p from 'es6-promise';
-(e6p as any).polyfill();
+import 'babel-polyfill';
 import 'isomorphic-fetch';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 const { Router, browserHistory } = require('react-router');
