9795e5e0fe4a2b59ca24cdd49ce5517b5479becf
drimpact <drimpact@users.noreply.github.com>
2018-08-31 16:52:12 +0100

    explaining what a question category is and moving to category instead of question category

--- src/app/containers/OutcomeSet/categories.tsx
+++ src/app/containers/OutcomeSet/categories.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {IOutcomeResult, getOutcomeSet} from 'apollo/modules/outcomeSets';
 import {CategoryList} from 'components/CategoryList';
+import {Message, Icon} from 'semantic-ui-react';
+const strings = require('./../../../strings.json');
 
 interface IProps {
   data: IOutcomeResult;
@@ -12,7 +14,15 @@ interface IProps {
 }
 
 const CategoriesInner = (p: IProps) => {
-  return (<CategoryList outcomeSetID={p.match.params.id} data={p.data}/>);
+  return (
+    <div>
+      <Message info={true}>
+        <Icon name="question" />
+        {strings.questionCategoryExplanation}
+      </Message>
+      <CategoryList outcomeSetID={p.match.params.id} data={p.data}/>
+    </div>
+  );
 };
 
 export const Categories = getOutcomeSet<IProps>((props) => props.match.params.id)(CategoriesInner);
