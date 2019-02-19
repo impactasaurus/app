5b03dfe70949ae3bc4af9e3e146ef3678b2f2052
drimpact <drimpact@users.noreply.github.com>
2019-02-19 22:29:39 +0000

    fixing missed class usage

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -74,7 +74,7 @@ export class Header extends React.Component<IProps, any> {
             <Icon name="plus" className="required" />
           </Menu.Item>
 
-          <Menu.Menu class="right" position="right">
+          <Menu.Menu className="right" position="right">
             <Menu.Item id="twitter-menu-link" href="https://twitter.com/impactasaurus" target="blank" className="icon-menu-item">
               <TwitterIcon/>
             </Menu.Item>
