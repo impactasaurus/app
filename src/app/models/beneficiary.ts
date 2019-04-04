862323aa691363a52162305999d7d0bd63b113cb
drimpact <drimpact@users.noreply.github.com>
2019-04-04 21:20:48 +0100

    adding getBeneficiary request

---
+++ src/app/models/beneficiary.ts
@@ -0,0 +1,12 @@
+import {gql} from 'react-apollo';
+
+export interface IBeneficiary {
+  ID: string;
+  tags: string[];
+}
+
+export const fragment = gql`
+  fragment defaultBeneficiary on Beneficiary {
+    id
+    tags
+  }`;
