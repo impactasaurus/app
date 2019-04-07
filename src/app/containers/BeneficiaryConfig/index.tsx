3c5fada1ce8c86a13ac9940ce52ecfc28dcd696c
drimpact <drimpact@users.noreply.github.com>
2019-04-07 21:47:56 +0100

    adding beneficiary config page with button in beneficiary signpost menu

---
+++ src/app/containers/BeneficiaryConfig/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+class BeneficiaryConfig extends React.Component<any, any> {
+
+  constructor(props) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <div>
+        Settings
+      </div>
+    );
+  }
+}
+
+export { BeneficiaryConfig };
