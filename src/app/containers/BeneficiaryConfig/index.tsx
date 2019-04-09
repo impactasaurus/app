2f8c4c2a93caadebe14346ead584a5a00821e057
drimpact <drimpact@users.noreply.github.com>
2019-04-09 21:56:41 +0100

    adding beneficiary form to beneficiary config page, still need to implement saving to server

--- src/app/containers/BeneficiaryConfig/index.tsx
+++ src/app/containers/BeneficiaryConfig/index.tsx
@@ -1,18 +1,60 @@
 import * as React from 'react';
+import {BeneficiaryForm, IFormOutput} from 'components/BeneficiaryForm';
+import {IBeneficiaryResult, getBeneficiary} from 'apollo/modules/beneficiaries';
+import {ApolloLoaderHoC} from '../../components/ApolloLoaderHoC';
 
-class BeneficiaryConfig extends React.Component<any, any> {
+interface IProps {
+  match: {
+    params: {
+      id: string,
+    },
+  };
+  data?: IBeneficiaryResult;
+}
+
+interface IState {
+  formVersion: number;
+}
+
+const getBen = (p: IProps): string => p.match.params.id;
+
+class BeneficiaryConfigInner extends React.Component<IProps, IState> {
 
   constructor(props) {
     super(props);
+    this.state = {
+      formVersion: 0,
+    };
+    this.onSave = this.onSave.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+  }
+
+  private onSave(v: IFormOutput): Promise<void> {
+    console.log(v);
+    return Promise.resolve();
+  }
+
+  private onCancel() {
+    this.setState({
+      formVersion: this.state.formVersion + 1,
+    });
   }
 
   public render() {
     return (
       <div>
-        Settings
+        <BeneficiaryForm
+          beneficiaryID={getBen(this.props)}
+          onFormSubmit={this.onSave}
+          onCancel={this.onCancel}
+          tags={this.props.data.getBeneficiary.tags}
+          key={this.state.formVersion}
+        />
       </div>
     );
   }
 }
 
+const BeneficiaryConfigWithLoader = ApolloLoaderHoC<IProps>('beneficiary', (p: IProps) => p.data, BeneficiaryConfigInner);
+const BeneficiaryConfig = getBeneficiary<IProps>(getBen)(BeneficiaryConfigWithLoader);
 export { BeneficiaryConfig };
