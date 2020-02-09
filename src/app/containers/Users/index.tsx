096c84fdca78ec41645061df172f224099d16261
drimpact <drimpact@users.noreply.github.com>
2020-02-09 11:56:44 +0000

    #405 using page wrapper for users page

--- src/app/containers/Users/index.tsx
+++ src/app/containers/Users/index.tsx
@@ -1,25 +1,15 @@
 import * as React from 'react';
-import { Helmet } from 'react-helmet';
-import { Grid } from 'semantic-ui-react';
 import {InviteGenerator} from 'components/InviteGenerator';
 import {OrganisationUsers} from 'components/OrganisationUsers';
+import {PageWrapperHoC} from 'components/PageWrapperHoC';
 
-class Users extends React.Component<any, any> {
+export const UsersInner = () => (
+  <div>
+    <OrganisationUsers />
+    <InviteGenerator />
+  </div>
+);
 
-  public render() {
-    return (
-      <Grid container={true} columns={1} id="users">
-        <Grid.Column>
-          <Helmet>
-            <title>Users</title>
-          </Helmet>
-          <h1>Users</h1>
-          <OrganisationUsers />
-          <InviteGenerator />
-        </Grid.Column>
-      </Grid>
-    );
-  }
-}
+const Users = PageWrapperHoC('Users', 'users', UsersInner);
 
 export { Users };
