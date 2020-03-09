f41872dd3e021881560fdb132c87c885bc57138a
drimpact <drimpact@users.noreply.github.com>
2020-03-09 19:44:36 +0000

    #511 adjusting tick styling and pull focus onto first incomplete item in the list

--- src/app/components/OnboardingChecklist/tick.tsx
+++ src/app/components/OnboardingChecklist/tick.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Icon} from 'semantic-ui-react';
 import './tick.less';
 
 interface IProps {
@@ -8,23 +9,8 @@ interface IProps {
 
 export const Tick = (p: IProps) => {
   const wrapper = (inner: JSX.Element): JSX.Element => <div className="tick-wrapper">{inner} </div>;
-  if (p.loading === true) {
-    return wrapper(<div key="loading" className="tick f-modal-icon f-modal-loading scaleLoading" />);
-  }
-  if (p.complete === true) {
-    return wrapper((
-      <div key="complete" className="tick f-modal-icon f-modal-success animate">
-        <span className="f-modal-line f-modal-tip animateSuccessTip"/>
-        <span className="f-modal-line f-modal-long animateSuccessLong"/>
-        <div className="f-modal-placeholder"/>
-        <div className="f-modal-fix"/>
-      </div>
-    ));
-  }
+  const className = p.complete ? 'complete' : 'incomplete';
   return wrapper((
-    <div key="incomplete" className="tick f-modal-icon f-modal-success">
-      <div className="f-modal-placeholder"/>
-      <div className="f-modal-fix"/>
-    </div>
+    <Icon aria-hidden="true" name="check circle" className={className}/>
   ));
 };
