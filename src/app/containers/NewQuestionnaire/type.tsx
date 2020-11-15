6050b93e2effd8525db87dfe2a016d635e2540d1
drimpact <drimpact@users.noreply.github.com>
2020-11-15 21:16:02 +0000

    #544 adding quick start component to new questionnaire wizard

--- src/app/containers/NewQuestionnaire/type.tsx
+++ src/app/containers/NewQuestionnaire/type.tsx
@@ -3,17 +3,34 @@ import * as React from 'react';
 import {Item, MultiChoice} from 'components/MultiChoice';
 import {IURLConnector, setURL} from 'redux/modules/url';
 import {PageWrapperHoC} from 'components/PageWrapperHoC';
+import { Segment } from 'semantic-ui-react';
+import {ICatalogueImport, importQuestionnaire} from '../../apollo/modules/catalogue';
+import {allOutcomeSets, IOutcomeResult} from '../../apollo/modules/outcomeSets';
+const RocketIcon = require('./rocket.inline.svg');
 const { connect } = require('react-redux');
+
+interface IProps extends ICatalogueImport, IURLConnector {
+  data?: IOutcomeResult;
+}
+interface IState {
+  error: boolean;
+  importing: boolean;
+}
 
 @connect(undefined, (dispatch) => ({
   setURL: bindActionCreators(setURL, dispatch),
 }))
-class NewQuestionnaireTypSelectionInner extends React.Component<IURLConnector, any> {
+class NewQuestionnaireTypSelectionInner extends React.Component<IProps, IState> {
 
   constructor(props) {
     super(props);
     this.goToCatalogue = this.goToCatalogue.bind(this);
     this.goToCustom = this.goToCustom.bind(this);
+    this.quickStart = this.quickStart.bind(this);
+    this.state = {
+      importing: false,
+      error: false,
+    };
   }
 
   private goToCatalogue() {
@@ -22,6 +39,38 @@ class NewQuestionnaireTypSelectionInner extends React.Component<IURLConnector, a
 
   private goToCustom() {
     this.props.setURL(`/questions/new/custom`);
+  }
+
+  private quickStart() {
+    this.setState({
+      importing: true,
+      error: false,
+    });
+    this.props.importQuestionnaire('eac0c7b4-c9af-4475-94b8-72c548ea9588')
+    .then(() => {
+      this.props.setURL('/questions');
+    })
+    .catch(() => {
+      this.setState({
+        importing: false,
+        error: true,
+      });
+    });
+  }
+
+  private renderQuickStart(data: IOutcomeResult) {
+    if(data.loading !== false || (data.allOutcomeSets && data.allOutcomeSets.length > 0)) {
+      return <div key="noQuickStart" />;
+    }
+    return (
+      <Segment key="quickStart" id="quick-start" raised={true} compact={true} style={{marginLeft:'auto',marginRight:'auto'}}>
+        <h3>
+          <RocketIcon style={{width:'1rem', marginRight:'.3rem'}}/>
+          Quick start
+        </h3>
+        <p><a onClick={this.quickStart}>Click here to add the ONS Wellbeing questionnaire</a> which is perfect for trying out Impactasaurus</p>
+      </Segment>
+    );
   }
 
   public render() {
@@ -34,8 +83,11 @@ class NewQuestionnaireTypSelectionInner extends React.Component<IURLConnector, a
       subtitle: 'Select a questionnaire from the literature',
       onClick: this.goToCatalogue,
     }];
-    return <MultiChoice items={items} />;
+    return [
+        this.renderQuickStart(this.props.data),
+        <MultiChoice key="choice" items={items} />,
+    ];
   }
 }
 
-export const NewQuestionnaireTypeSelector = PageWrapperHoC('New Questionnaire', 'new-questionnaire', NewQuestionnaireTypSelectionInner);
+export const NewQuestionnaireTypeSelector = PageWrapperHoC('New Questionnaire', 'new-questionnaire', allOutcomeSets(importQuestionnaire<IProps>(NewQuestionnaireTypSelectionInner)));
