b8d80bd22313316727a22d1bd7c9c5f919b7214d
drimpact <drimpact@users.noreply.github.com>
2021-01-19 21:31:51 +0000

    #560 adding logo branding support and a custom logo for localhost

--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -2,13 +2,53 @@ import * as React from 'react';
 const SVG = require('./logo.inline.svg');
 import './style.less';
 
-const Logo =  () => {
-  return (
-    <span className="impactasaurus-logo">
-      <SVG />
-      <span className="title">Impactasaurus</span>
-    </span>
-  );
-};
+const subdomain = () => window.location.hostname.split('.')[0];
+
+class Logo extends React.Component<any, any> {
+
+  constructor(props) {
+    super(props);
+    const loadBranding = subdomain() !== 'app';
+    this.state = {
+      logo: null,
+      loadBranding,
+      subdomain: subdomain(),
+    };
+  }
+
+  public componentDidMount() {
+    const {loadBranding, subdomain} = this.state;
+    if(loadBranding) {
+      import(`./../../../branding/${subdomain}/${subdomain}.tsx`)
+      .then((m) => {
+        this.setState({
+          logo: m.default,
+        });
+      })
+      .catch(() => {
+        console.log(`no logo for subdomain '${subdomain}'`);
+        this.setState({
+          loadBranding: false,
+        });
+      });
+    }
+  }
+
+  public render() {
+    if(this.state.loadBranding) {
+      const BrandedLogo = this.state.logo;
+      if(BrandedLogo) {
+        return <BrandedLogo />;
+      }
+      return <span />;
+    }
+    return (
+      <span className="impactasaurus-logo">
+        <SVG />
+        <span className="title">Impactasaurus</span>
+      </span>
+    );
+  }
+}
 
 export default Logo;
