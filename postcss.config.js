24eaaf41119de98242fe2812d3c437b64032af38
drimpact <drimpact@users.noreply.github.com>
2021-01-23 19:56:10 +0000

    #567 adding ie11 polyfills

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
     plugins: [
       [
-        "postcss-preset-env",
-        {
-          // Options
+        "postcss-preset-env", {
+          browsers: [ ">0.5%", "not op_mini all", "ie 11"]
         },
       ],
     ],
