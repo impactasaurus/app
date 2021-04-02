3460bfdc1a03975ceb40ea147edffe2dc5a0eaf5
drimpact <drimpact@users.noreply.github.com>
2021-04-02 20:01:11 +0100

    #587 translating data entry and questionnaire review

--- src/app/components/QuestionnaireReview/index.tsx
+++ src/app/components/QuestionnaireReview/index.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {IMeetingMutation, completeMeeting} from 'apollo/modules/meetings';
 import {RecordQuestionSummary} from 'components/RecordQuestionSummary';
-import 'rc-slider/assets/index.css';
 import { Button, ButtonProps } from 'semantic-ui-react';
-import './style.less';
 import {IQuestion} from 'models/question';
 import {IMeeting} from 'models/meeting';
+import {useTranslation} from 'react-i18next';
+import 'rc-slider/assets/index.css';
+import './style.less';
 
 interface IProps extends IMeetingMutation {
   record: IMeeting;
@@ -14,81 +15,60 @@ interface IProps extends IMeetingMutation {
   onQuestionClick: (questionID: string) => void;
 }
 
-interface IState {
-  completing: boolean;
-  completeError: string;
-}
+const QuestionnaireReviewInner = (p: IProps) => {
+  const [completing, setCompleting] = useState(false);
+  const [completeError, setCompleteError] = useState<string>(undefined);
+  const {t} = useTranslation();
 
-class QuestionnaireReviewInner extends React.Component<IProps, IState> {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      completing: false,
-      completeError: undefined,
-    };
-    this.review = this.review.bind(this);
-    this.renderAnswerReview = this.renderAnswerReview.bind(this);
-    this.navigateToQuestion = this.navigateToQuestion.bind(this);
-  }
-
-  private review() {
-    this.setState({
-      completing: true,
-      completeError: undefined,
-    });
-    this.props.completeMeeting(this.props.record.id, this.props.record.beneficiary)
+  const review = () => {
+    setCompleting(true);
+    setCompleteError(undefined);
+    p.completeMeeting(p.record.id, p.record.beneficiary)
       .then(() => {
-        this.setState({
-          completing: false,
-          completeError: undefined,
-        });
-        this.props.onComplete();
+        setCompleting(false);
+        setCompleteError(undefined);
+        p.onComplete();
       })
       .catch((e: string) => {
-        this.setState({
-          completing: false,
-          completeError: e,
-        });
+        setCompleting(false);
+        setCompleteError(e);
       });
   }
 
-  private navigateToQuestion(selectedQ: IQuestion, _: number): Promise<void> {
-    this.props.onQuestionClick(selectedQ.id);
+  const navigateToQuestion = (selectedQ: IQuestion): Promise<void> => {
+    p.onQuestionClick(selectedQ.id);
     return Promise.resolve();
   }
 
-  private renderAnswerReview(): JSX.Element {
+  const renderAnswerReview = (): JSX.Element => {
     return (
       <div className="answer-review">
         <RecordQuestionSummary
-          recordID={this.props.record.id}
-          onQuestionClick={this.navigateToQuestion}
+          recordID={p.record.id}
+          onQuestionClick={navigateToQuestion}
         />
       </div>
     );
   }
 
-  public render() {
-    if (this.props.record === undefined) {
-      return (<div />);
-    }
-    const props: ButtonProps = {};
-    if (this.state.completing) {
-      props.loading = true;
-      props.disabled = true;
-    }
-    return (
-      <div id="questionnaire-review">
-        <h1>Review</h1>
-        <p>Please review your answers below. Once you are happy, click save, to mark the record as complete.</p>
-        {this.renderAnswerReview()}
-        <Button onClick={this.props.onBack}>Back</Button>
-        <Button {...props} onClick={this.review}>Save</Button>
-        <p>{this.state.completeError}</p>
-      </div>
-    );
+  if (p.record === undefined) {
+    return (<div />);
   }
+  const props: ButtonProps = {};
+  if (completing) {
+    props.loading = true;
+    props.disabled = true;
+  }
+  return (
+    <div id="questionnaire-review">
+      <h1>{t("Review")}</h1>
+      <p>{t("Please review your answers below. Once you are happy, click save, to mark the record as complete.")}</p>
+      {renderAnswerReview()}
+      <Button onClick={p.onBack}>{t("Back")}</Button>
+      <Button {...props} onClick={review}>{t("Save")}</Button>
+      <p>{completeError}</p>
+    </div>
+  );
 }
 const QuestionnaireReview = completeMeeting<IProps>(QuestionnaireReviewInner);
 export { QuestionnaireReview };
--- src/app/containers/DataEntry/index.tsx
+++ src/app/containers/DataEntry/index.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import { Helmet } from 'react-helmet';
+import React, {useState} from 'react';
 import {IMeetingResult, getMeeting, completeMeeting, IMeetingMutation} from 'apollo/modules/meetings';
-import 'rc-slider/assets/index.css';
-import {ButtonProps, Grid, Loader, Button} from 'semantic-ui-react';
-import { bindActionCreators } from 'redux';
-import {IURLConnector, setURL} from 'redux/modules/url';
+import {ButtonProps, Button} from 'semantic-ui-react';
+import {IURLConnector, UrlConnector} from 'redux/modules/url';
 import {IAnswer} from 'models/answer';
 import {IQuestion, Question} from 'models/question';
 import {QuestionInline} from 'components/QuestionInline';
 import {renderArray} from 'helpers/react';
 import {getHumanisedDateFromISO} from 'helpers/moment';
 import {QuestionnaireReview} from 'components/QuestionnaireReview';
-import {Error} from 'components/Error';
 import {MeetingNotepad} from 'components/MeetingNotepad';
-const { connect } = require('react-redux');
+import {connect} from 'react-redux';
+import { MinimalPageWrapperHoC } from 'components/PageWrapperHoC';
+import { ApolloLoaderHoC } from 'components/ApolloLoaderHoC';
+import { useTranslation } from 'react-i18next';
+import 'rc-slider/assets/index.css';
 
 interface IProps extends IURLConnector, IMeetingMutation {
   data: IMeetingResult;
@@ -24,13 +24,6 @@ interface IProps extends IURLConnector, IMeetingMutation {
   };
   questions?: IQuestion[];
   answers?: IAnswer[];
-}
-
-interface IState {
-    saving?: boolean;
-    completing?: boolean;
-    completeError?: string;
-    screen?: Screen;
 }
 
 enum Screen {
@@ -45,7 +38,97 @@ function answeredAllQuestions(p: IProps): boolean {
   }, true);
 }
 
-@connect((_, ownProps: IProps) => {
+const DataEntryInner = (p: IProps) => {
+  const [saving, setSavingInner] = useState(false);
+  const [screen, setScreen] = useState<Screen>(Screen.QUESTION);
+  const {t} = useTranslation();
+
+  const setPage = (page: Screen): () => void => {
+    return () => {
+      setScreen(page);
+    };
+  }
+
+  const completed = () => {
+    p.setURL(`/beneficiary/${p.data.getMeeting.beneficiary}`, `?q=${p.data.getMeeting.outcomeSetID}`);
+  }
+
+  const setSaving = (toSet: boolean) => {
+    return () => {
+      setSavingInner(toSet);
+    };
+  }
+
+  const renderQuestion = (q: Question, idx: number): JSX.Element => {
+    return (
+      <QuestionInline
+        key={q.id}
+        index={idx+1}
+        record={p.data.getMeeting}
+        questionID={q.id}
+        onSaving={setSaving(true)}
+        onSaved={setSaving(false)}
+        disabled={saving}
+      />
+    );
+  }
+
+  const renderQuestionPage = (): JSX.Element => {
+    const meeting = p.data.getMeeting;
+
+    const props: ButtonProps = {};
+    if (saving) {
+      props.disabled = true;
+    }
+    if (!answeredAllQuestions(p)) {
+      props.disabled = true;
+    }
+
+    return (
+      <div>
+        <h1>{meeting.beneficiary} - {getHumanisedDateFromISO(meeting.conducted)}</h1>
+        {renderArray(renderQuestion, p.questions)}
+        <Button {...props} onClick={setPage(Screen.NOTES)}>{t("Next")}</Button>
+      </div>
+    );
+  }
+
+  const renderFinished = (): JSX.Element => {
+    return(
+      <QuestionnaireReview
+        record={p.data.getMeeting}
+        onQuestionClick={setPage(Screen.QUESTION)}
+        onBack={setPage(Screen.NOTES)}
+        onComplete={completed}
+      />
+    );
+  }
+
+  const renderNotepad = (): JSX.Element => {
+    return (
+      <MeetingNotepad
+        record={p.data.getMeeting}
+        onComplete={setPage(Screen.REVIEW)}
+        onBack={setPage(Screen.QUESTION)}
+      />
+    );
+  }
+
+  const meeting = p.data.getMeeting;
+  if (meeting === undefined) {
+      return <div />;
+  }
+
+  if (screen === Screen.REVIEW) {
+    return renderFinished();
+  }
+  if (screen === Screen.NOTES) {
+    return renderNotepad();
+  }
+  return renderQuestionPage();
+}
+
+const propsPreprocessing = (_, ownProps: IProps) => {
   const out: any = {};
   if (ownProps.data !== undefined && ownProps.data.getMeeting !== undefined) {
     out.questions = (ownProps.data.getMeeting.outcomeSet.questions || [])
@@ -53,139 +136,12 @@ function answeredAllQuestions(p: IProps): boolean {
     out.answers = ownProps.data.getMeeting.answers;
   }
   return out;
-}, (dispatch) => ({
-  setURL: bindActionCreators(setURL, dispatch),
-}))
-class DataEntryInner extends React.Component<IProps, IState> {
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      saving: false,
-      screen: Screen.QUESTION,
-    };
-    this.completed = this.completed.bind(this);
-    this.setSaving = this.setSaving.bind(this);
-    this.completed = this.completed.bind(this);
-    this.setPage = this.setPage.bind(this);
-    this.renderQuestion = this.renderQuestion.bind(this);
-    this.renderFinished = this.renderFinished.bind(this);
-    this.renderQuestionPage = this.renderQuestionPage.bind(this);
-    this.renderNotepad = this.renderNotepad.bind(this);
-  }
-
-  private setPage(page: Screen): () => void {
-    return () => {
-      this.setState({
-        screen: page,
-      });
-    };
-  }
-
-  private completed() {
-    this.props.setURL(`/beneficiary/${this.props.data.getMeeting.beneficiary}`, `?q=${this.props.data.getMeeting.outcomeSetID}`);
-  }
-
-  private setSaving(toSet: boolean) {
-    return () => {
-      this.setState({
-        saving: toSet,
-      });
-    };
-  }
-
-  private renderQuestion(q: Question, idx: number): JSX.Element {
-    return (
-      <QuestionInline
-        key={q.id}
-        index={idx+1}
-        record={this.props.data.getMeeting}
-        questionID={q.id}
-        onSaving={this.setSaving(true)}
-        onSaved={this.setSaving(false)}
-        disabled={this.state.saving}
-      />
-    );
-  }
-
-  private renderQuestionPage(): JSX.Element {
-    const meeting = this.props.data.getMeeting;
-
-    const props: ButtonProps = {};
-    if (this.state.completing) {
-      props.loading = true;
-      props.disabled = true;
-    }
-    if (this.state.saving) {
-      props.disabled = true;
-    }
-    if (!answeredAllQuestions(this.props)) {
-      props.disabled = true;
-    }
-
-    return (
-      <div>
-        <h1>{meeting.beneficiary} - {getHumanisedDateFromISO(meeting.conducted)}</h1>
-        {renderArray(this.renderQuestion, this.props.questions)}
-        <Button {...props} onClick={this.setPage(Screen.NOTES)}>Next</Button>
-        <p>{this.state.completeError}</p>
-      </div>
-    );
-  }
-
-  private renderFinished(): JSX.Element {
-    return(
-      <QuestionnaireReview
-        record={this.props.data.getMeeting}
-        onQuestionClick={this.setPage(Screen.QUESTION)}
-        onBack={this.setPage(Screen.NOTES)}
-        onComplete={this.completed}
-      />
-    );
-  }
-
-  private renderNotepad(): JSX.Element {
-    return (
-      <MeetingNotepad
-        record={this.props.data.getMeeting}
-        onComplete={this.setPage(Screen.REVIEW)}
-        onBack={this.setPage(Screen.QUESTION)}
-      />
-    );
-  }
-
-  public render() {
-    const wrapper = (inner: JSX.Element): JSX.Element => {
-      return (
-        <Grid container={true} columns={1} id="data-entry">
-          <Grid.Column>
-            <Helmet>
-              <title>Data Entry</title>
-            </Helmet>
-            <div>
-              {inner}
-            </div>
-          </Grid.Column>
-        </Grid>
-      );
-    };
-
-    if (this.props.data.error) {
-      return wrapper(<Error text="Failed to load" />);
-    }
-    const meeting = this.props.data.getMeeting;
-    if (meeting === undefined) {
-        return wrapper(<Loader active={true} inline="centered" />);
-    }
-
-    if (this.state.screen === Screen.REVIEW) {
-      return wrapper(this.renderFinished());
-    }
-    if (this.state.screen === Screen.NOTES) {
-      return wrapper(this.renderNotepad());
-    }
-    return wrapper(this.renderQuestionPage());
-  }
-}
-const DataEntry = completeMeeting<IProps>(getMeeting<IProps>((props) => props.match.params.id)(DataEntryInner));
+const DataEntryConnected = connect(propsPreprocessing, UrlConnector)(DataEntryInner);
+// t("questionnaire")
+const DataEntryLoader = ApolloLoaderHoC("questionnaire", (p: IProps) => p.data, DataEntryConnected);
+const DataEntryWithData = completeMeeting<IProps>(getMeeting<IProps>((props) => props.match.params.id)(DataEntryLoader));
+// t("Data Entry")
+const DataEntry = MinimalPageWrapperHoC("Data Entry", "data-entry", DataEntryWithData);
 export { DataEntry };
