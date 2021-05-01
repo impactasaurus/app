b7a9a187b9c8b8adad4f8595fbad26d755937806
drimpact <drimpact@users.noreply.github.com>
2021-05-01 19:52:09 +0100

    #591 localising dates and times using moment

--- src/app/components/Localiser/index.tsx
+++ src/app/components/Localiser/index.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {IGetSelf, getSelf} from 'apollo/modules/user';
 import { useTranslation } from 'react-i18next';
+import moment from 'moment';
 
 interface IProps {
   self?: IGetSelf;
@@ -13,12 +14,17 @@ const Inner = (p: IProps) => {
     if(!p.self.loading && !p.self.error &&
       p.self.getSelf && p.self.getSelf.settings) {
       const lng = p.self.getSelf.settings.language;
+      let prom;
       if (lng && lng !== "und") {
-        i18n.changeLanguage(lng);
+        prom = i18n.changeLanguage(lng);
       } else {
         // use language detector
-        i18n.changeLanguage();
+        prom = i18n.changeLanguage();
       }
+      prom.finally(() => {
+        const lan = i18n.language === "tlh" ? "x-pseudo" : i18n.language;
+        moment.locale(lan);
+      });
     }
   }, [p.self]);
 
