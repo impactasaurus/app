19a30ad66197c06b77b782d5f01d848792910cf0
drimpact <drimpact@users.noreply.github.com>
2022-04-24 15:24:57 +0100

    #703 shifting signup page logic to route permissions

--- src/app/containers/Signup/index.tsx
+++ src/app/containers/Signup/index.tsx
@@ -1,21 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigator } from "redux/modules/url";
 import { PageWrapperHoC } from "components/PageWrapperHoC";
 import { ISignup, signup } from "../../apollo/modules/organisation";
 import { IFormOutput, SignupForm } from "../../components/SignupForm";
-import { useUser } from "redux/modules/user";
 
 type IProps = ISignup;
 
 const SignupInner = (p: IProps): JSX.Element => {
-  const { loggedIn } = useUser();
   const setURL = useNavigator();
-
-  useEffect(() => {
-    if (loggedIn) {
-      setURL("/");
-    }
-  }, [loggedIn]);
 
   const onSubmit = (v: IFormOutput): Promise<void> => {
     return p.signup(v.name, v.email, v.password, v.organisation).then(() => {
