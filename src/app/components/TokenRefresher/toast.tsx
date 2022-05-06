39e9240bd837d8bb4b93ed5ea4d7867ecb82ce25
drimpact <drimpact@users.noreply.github.com>
2022-05-06 20:28:37 +0100

    #678 improving token refresh toast messaging

--- src/app/components/TokenRefresher/toast.tsx
+++ src/app/components/TokenRefresher/toast.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import { timeToExpiry } from "helpers/auth";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useLogout } from "redux/modules/user";
+import { useLogout, useSession } from "redux/modules/user";
 import { Button } from "semantic-ui-react";
+
+const secondsToExpiry = (expiry: Date) =>
+  Math.floor(timeToExpiry(expiry) / 1000);
 
 export const RefreshFailedToast = (): JSX.Element => {
   const { t } = useTranslation();
+  const { expiry } = useSession();
   const logout = useLogout();
+  const [secondsLeft, setSecondsLeft] = useState(secondsToExpiry(expiry));
+
+  useEffect(() => {
+    const ticker = window.setInterval(() => {
+      setSecondsLeft(secondsToExpiry(expiry));
+    }, 1000);
+    return () => {
+      window.clearInterval(ticker);
+    };
+  }, []);
 
   return (
     <div>
       <div style={{ marginBottom: "0.6em" }}>
-        {t("Your session is about to expire")}
+        {t(
+          "Your session will expire in {x} seconds, you will need to log in again",
+          {
+            x: secondsLeft,
+          }
+        )}
       </div>
       <Button compact={true} primary={true} onClick={() => logout()}>
-        {t("Reauthenticate")}
+        {t("Log in")}
       </Button>
     </div>
   );
