e9b2f52e7ade1e983c31967c3049064e3c04a18a
drimpact <drimpact@users.noreply.github.com>
2022-05-14 19:07:48 +0100

    #737 log out beneficiary after questionnaire is finished

--- src/app/containers/Meeting/thanks.tsx
+++ src/app/containers/Meeting/thanks.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useSetJWT } from "redux/modules/user";
 
 export const Thanks = (): JSX.Element => {
   const { t } = useTranslation();
+  const setJWT = useSetJWT();
+
+  useEffect(() => {
+    // log the beneficiary out once the record is complete
+    setJWT(null);
+  }, []);
+
   return (
     <div>
       <h1>{t("Thank you")}</h1>
