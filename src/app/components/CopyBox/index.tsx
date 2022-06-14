979179520bdacce34e0e5fccce7b179016841da2
drimpact <drimpact@users.noreply.github.com>
2022-06-14 20:18:44 +0100

    #744 providing more feedback when copy button is clicked

--- src/app/components/CopyBox/index.tsx
+++ src/app/components/CopyBox/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import useInterval from "helpers/hooks/useInterval";
 import { useTranslation } from "react-i18next";
 import { ButtonProps, Input } from "semantic-ui-react";
 
@@ -7,21 +8,33 @@ interface IProps {
   loading?: boolean;
 }
 
+const showCopiedLabelDuration = 4000;
+
 export const CopyBox = (p: IProps): JSX.Element => {
   const { t } = useTranslation();
+  const [copiedTime, setCopiedTime] = useState<number>(0);
   const linkInput: React.RefObject<Input> = React.createRef();
+
+  useInterval(() => {
+    if (copiedTime !== 0 && Date.now() - copiedTime > showCopiedLabelDuration) {
+      // force refresh
+      setCopiedTime(0);
+    }
+  }, 200);
 
   const copyLink = () => {
     const node: Input = linkInput.current;
     node.select();
     document.execCommand("copy");
+    setCopiedTime(Date.now());
   };
 
+  const copied = Date.now() - copiedTime < showCopiedLabelDuration;
   const action: ButtonProps = {
     primary: true,
     labelPosition: "right",
-    icon: "copy",
-    content: t("Copy"),
+    icon: copied ? "check" : "copy",
+    content: copied ? t("Copied") : t("Copy"),
     onClick: copyLink,
   };
 
