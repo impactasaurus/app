1c5f01a8f2a775888a5ee4d7be295bb6518a0f5a
Mohammad Ali Khan <mohammad_ali93@live.com>
2022-08-09 19:45:22 +0100

    Convert Meeting Graph into functional component (#762)
    Aside from the conversion, all the generic functions have been moved out to be in module scope. This way, these functions don't need to be re-created on each render

--- src/app/components/MeetingGraph/index.tsx
+++ src/app/components/MeetingGraph/index.tsx
@@ -20,116 +20,113 @@ interface ISeriesToDataMap {
   [series: string]: IGraphDataPoint[];
 }
 
-class MeetingGraph extends React.Component<IProp, any> {
-  private combineGraphSeries(series: IGraphSeries[]): IGraphSeries[] {
-    const seriesMap = series.reduce(
-      (m: ISeriesToDataMap, d: IGraphSeries): ISeriesToDataMap => {
-        if (m[d.label] === undefined) {
-          m[d.label] = [];
-        }
-        m[d.label].push(...d.data);
-        return m;
-      },
-      {}
-    );
-    const result: IGraphSeries[] = [];
-    for (const series in seriesMap) {
-      if (Object.prototype.hasOwnProperty.call(seriesMap, series)) {
-        result.push({
-          label: series,
-          data: seriesMap[series],
-        });
+const combineGraphSeries = (series: IGraphSeries[]): IGraphSeries[] => {
+  const seriesMap = series.reduce(
+    (m: ISeriesToDataMap, d: IGraphSeries): ISeriesToDataMap => {
+      if (m[d.label] === undefined) {
+        m[d.label] = [];
       }
+      m[d.label].push(...d.data);
+      return m;
+    },
+    {}
+  );
+  const result: IGraphSeries[] = [];
+  for (const series in seriesMap) {
+    if (Object.prototype.hasOwnProperty.call(seriesMap, series)) {
+      result.push({
+        label: series,
+        data: seriesMap[series],
+      });
     }
-    return result;
+  }
+  return result;
+};
+
+const getQuestionLevelData = (meetings: IMeeting[]): GraphData => {
+  const seriesPerDP = meetings.reduce(
+    (prevData: IGraphSeries[], meeting): IGraphSeries[] => {
+      const series = meeting.answers.reduce<IGraphSeries[]>(
+        (series: IGraphSeries[], answer: Answer): IGraphSeries[] => {
+          const q = meeting.outcomeSet.questions.find(
+            (q) => q.id === answer.questionID
+          );
+          if (q === undefined || q.archived) {
+            return series;
+          }
+          return series.concat({
+            label: q.short || q.question,
+            data: [
+              {
+                x: new Date(meeting.conducted),
+                y: answer.answer,
+              },
+            ],
+          });
+        },
+        []
+      );
+      return prevData.concat(series);
+    },
+    []
+  );
+  return {
+    series: combineGraphSeries(seriesPerDP),
+    scaleMax: getMaxQuestionValue(meetings[0].outcomeSet),
+    scaleMin: getMinQuestionValue(meetings[0].outcomeSet),
+  };
+};
+
+const getCategoryLevelData = (meetings: IMeeting[]): GraphData => {
+  const seriesPerDP = meetings.reduce(
+    (prevData: IGraphSeries[], meeting): IGraphSeries[] => {
+      return prevData.concat(
+        meeting.aggregates.category.map((categoryAg): IGraphSeries => {
+          let category = "Unknown Category";
+          const cat = meeting.outcomeSet.categories.find(
+            (c) => c.id === categoryAg.categoryID
+          );
+          if (cat !== undefined) {
+            category = cat.name;
+          }
+          return {
+            label: category,
+            data: [
+              {
+                x: new Date(meeting.conducted),
+                y: categoryAg.value,
+              },
+            ],
+          };
+        })
+      );
+    },
+    []
+  );
+  return {
+    series: combineGraphSeries(seriesPerDP),
+    scaleMax: getMaxCategoryValue(meetings[0].outcomeSet),
+    scaleMin: getMinCategoryValue(meetings[0].outcomeSet),
+  };
+};
+
+const MeetingGraph = (props: IProp): JSX.Element => {
+  const { meetings, aggregation } = props;
+
+  if (!Array.isArray(meetings) || meetings.length === 0) {
+    return <div />;
   }
 
-  private getQuestionLevelData(meetings: IMeeting[]): GraphData {
-    const seriesPerDP = meetings.reduce(
-      (prevData: IGraphSeries[], meeting): IGraphSeries[] => {
-        const series = meeting.answers.reduce<IGraphSeries[]>(
-          (series: IGraphSeries[], answer: Answer): IGraphSeries[] => {
-            const q = meeting.outcomeSet.questions.find(
-              (q) => q.id === answer.questionID
-            );
-            if (q === undefined || q.archived) {
-              return series;
-            }
-            return series.concat({
-              label: q.short || q.question,
-              data: [
-                {
-                  x: new Date(meeting.conducted),
-                  y: answer.answer,
-                },
-              ],
-            });
-          },
-          []
-        );
-        return prevData.concat(series);
-      },
-      []
-    );
-    return {
-      series: this.combineGraphSeries(seriesPerDP),
-      scaleMax: getMaxQuestionValue(meetings[0].outcomeSet),
-      scaleMin: getMinQuestionValue(meetings[0].outcomeSet),
-    };
-  }
+  const data =
+    aggregation === Aggregation.CATEGORY
+      ? getCategoryLevelData(meetings)
+      : getQuestionLevelData(meetings);
 
-  private getCategoryLevelData(meetings: IMeeting[]): GraphData {
-    const seriesPerDP = meetings.reduce(
-      (prevData: IGraphSeries[], meeting): IGraphSeries[] => {
-        return prevData.concat(
-          meeting.aggregates.category.map((categoryAg): IGraphSeries => {
-            let category = "Unknown Category";
-            const cat = meeting.outcomeSet.categories.find(
-              (c) => c.id === categoryAg.categoryID
-            );
-            if (cat !== undefined) {
-              category = cat.name;
-            }
-            return {
-              label: category,
-              data: [
-                {
-                  x: new Date(meeting.conducted),
-                  y: categoryAg.value,
-                },
-              ],
-            };
-          })
-        );
-      },
-      []
-    );
-    return {
-      series: this.combineGraphSeries(seriesPerDP),
-      scaleMax: getMaxCategoryValue(meetings[0].outcomeSet),
-      scaleMin: getMinCategoryValue(meetings[0].outcomeSet),
-    };
-  }
-
-  public render() {
-    if (
-      !Array.isArray(this.props.meetings) ||
-      this.props.meetings.length === 0
-    ) {
-      return <div />;
-    }
-
-    const data =
-      this.props.aggregation === Aggregation.CATEGORY
-        ? this.getCategoryLevelData(this.props.meetings)
-        : this.getQuestionLevelData(this.props.meetings);
-
-    return (
-      <div className="meeting-graph">
-        <Graph data={data} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="meeting-graph">
+      <Graph data={data} />
+    </div>
+  );
+};
 
 export { MeetingGraph };
