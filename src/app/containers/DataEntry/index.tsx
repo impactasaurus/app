4d272380e78b5062c02e06f324f5c197a2cd0680
drimpact <drimpact@users.noreply.github.com>
2022-08-26 21:56:59 +0100

    #773 refactoring data entry before changes

--- src/app/containers/DataEntry/index.tsx
+++ src/app/containers/DataEntry/index.tsx
@@ -7,31 +7,29 @@ import {
   setMeetingNotes,
   ISetMeetingNotes,
 } from "apollo/modules/meetings";
-import { IURLConnector, UrlConnector } from "redux/modules/url";
+import { useNavigator } from "redux/modules/url";
 import { IAnswer } from "models/answer";
 import { IQuestion, Question } from "models/question";
 import { QuestionInline } from "components/QuestionInline";
 import { renderArray } from "helpers/react";
-import { connect } from "react-redux";
 import { MinimalPageWrapperHoC } from "components/PageWrapperHoC";
 import { ApolloLoaderHoC } from "components/ApolloLoaderHoC";
 import { useTranslation } from "react-i18next";
 import { ISODateString } from "components/Moment";
 import { Notepad } from "components/Notepad";
-import { Button, ButtonProps } from "semantic-ui-react";
+import { Button, ButtonProps, Loader } from "semantic-ui-react";
 import { Error } from "components/Error";
 import { IOutcomeSet } from "models/outcomeSet";
+import { getQuestions } from "helpers/questionnaire";
 import "rc-slider/assets/index.css";
 
-interface IProps extends IURLConnector, IMeetingMutation, ISetMeetingNotes {
+interface IProps extends IMeetingMutation, ISetMeetingNotes {
   data: IMeetingResult;
   match: {
     params: {
       id: string;
     };
   };
-  questions?: IQuestion[];
-  answers?: IAnswer[];
 }
 
 const completedQuestionnaire = (
@@ -60,6 +58,7 @@ const DataEntryInner = (p: IProps) => {
   const [savingError, setSavingError] = useState<boolean>(false);
   const [notes, setNotes] = useState<string>(p.data.getMeeting.notes);
   const { t } = useTranslation();
+  const setURL = useNavigator();
 
   const completed = () => {
     const ben = p.data.getMeeting.beneficiary;
@@ -74,7 +73,10 @@ const DataEntryInner = (p: IProps) => {
       .then(() => {
         setSavingInner(false);
         setSavingError(false);
-        p.setURL(`/beneficiary/${ben}`, `?q=${p.data.getMeeting.outcomeSetID}`);
+        setURL(
+          `/beneficiary/${ben}`,
+          new URLSearchParams({ q: questionnaire.id })
+        );
       })
       .catch((e: string | Error) => {
         console.error(e);
@@ -103,78 +105,63 @@ const DataEntryInner = (p: IProps) => {
     );
   };
 
-  const renderQuestionPage = (): JSX.Element => {
-    const meeting = p.data.getMeeting;
-    const placeholder = t("Record any additional comments, goals or actions");
-    const saveProps: ButtonProps = {};
-    if (saving) {
-      saveProps.loading = true;
-      saveProps.disabled = true;
-    }
-    if (
-      !completedQuestionnaire(p.questions, p.answers, meeting.outcomeSet, notes)
-    ) {
-      saveProps.disabled = true;
-    }
-    return (
-      <div>
-        <h1>
-          {meeting.beneficiary} - <ISODateString iso={meeting.conducted} />
-        </h1>
-        {renderArray(renderQuestion, p.questions)}
-        {meeting.outcomeSet?.noteDeactivated !== true && (
-          <>
-            <h3>{t("Additional Comments")}</h3>
-            <Notepad
-              onChange={setNotes}
-              notes={notes}
-              prompt={meeting.outcomeSet?.notePrompt || false}
-              placeholder={
-                meeting.outcomeSet?.notePrompt ? undefined : placeholder
-              }
-              required={meeting.outcomeSet?.noteRequired}
-            />
-          </>
-        )}
-        <Button
-          {...saveProps}
-          onClick={completed}
-          style={{ marginTop: "20px" }}
-        >
-          {t("Save")}
-        </Button>
-        {savingError && <Error text={t("Failed to finalise record")} />}
-      </div>
-    );
-  };
-
-  const meeting = p.data.getMeeting;
-  if (meeting === undefined) {
-    return <div />;
+  if (p.data.error) {
+    return <Error text={t("Failed to load")} />;
   }
-  return renderQuestionPage();
+
+  const questionnaire = p.data?.getMeeting?.outcomeSet;
+  const questions = questionnaire ? getQuestions(questionnaire) : undefined;
+  const meeting = p.data?.getMeeting;
+  const answers = meeting?.answers;
+
+  if (
+    p.data.loading ||
+    meeting === undefined ||
+    meeting.id != p.match.params.id
+  ) {
+    return <Loader active={true} inline="centered" />;
+  }
+
+  const placeholder = t("Record any additional comments, goals or actions");
+  const saveProps: ButtonProps = {};
+  if (saving) {
+    saveProps.loading = true;
+    saveProps.disabled = true;
+  }
+  if (!completedQuestionnaire(questions, answers, questionnaire, notes)) {
+    saveProps.disabled = true;
+  }
+  return (
+    <div>
+      <h1>
+        {meeting.beneficiary} - <ISODateString iso={meeting.conducted} />
+      </h1>
+      {renderArray(renderQuestion, questions)}
+      {questionnaire?.noteDeactivated !== true && (
+        <>
+          <h3>{t("Additional Comments")}</h3>
+          <Notepad
+            onChange={setNotes}
+            notes={notes}
+            prompt={questionnaire?.notePrompt || false}
+            placeholder={questionnaire?.notePrompt ? undefined : placeholder}
+            required={questionnaire?.noteRequired}
+          />
+        </>
+      )}
+      <Button {...saveProps} onClick={completed} style={{ marginTop: "20px" }}>
+        {t("Save")}
+      </Button>
+      {savingError && <Error text={t("Failed to finalise record")} />}
+    </div>
+  );
 };
 
-const propsPreprocessing = (_, ownProps: IProps) => {
-  const out: any = {};
-  if (ownProps.data !== undefined && ownProps.data.getMeeting !== undefined) {
-    out.questions = (
-      ownProps.data.getMeeting.outcomeSet.questions || []
-    ).filter((q) => !q.archived);
-    out.answers = ownProps.data.getMeeting.answers;
-  }
-  return out;
-};
-
-const DataEntryConnected = connect(
-  propsPreprocessing,
-  UrlConnector
-)(DataEntryInner);
 // t("questionnaire")
 const DataEntryLoader = ApolloLoaderHoC(
   "questionnaire",
   (p: IProps) => p.data,
-  DataEntryConnected
+  DataEntryInner
 );
 const DataEntryWithData = setMeetingNotes<IProps>(
   completeMeeting<IProps>(
